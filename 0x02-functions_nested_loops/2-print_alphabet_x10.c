c983d413f30620548cb444f3fbeb29756e37fc5f
quedjo <quedjofrempong@gmail.com>
2023-02-22 02:53:27 -0800

    alpha 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**print_alphabet_x10 - prints alphabets twice
+ * Return: null
+ */
+
+void print_alphabet_x10(void)
+{
+        int i;
+	int count = 0;
+
+        while (count < 10)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+        {
+        _putchar(i);
+        }
+	count++
+	}
+        _putchar('\n');
+}
