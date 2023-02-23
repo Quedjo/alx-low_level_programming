702ec949c09e00d0a53a991b00eefc7c0bec4987
quedjo <quedjofrempong@gmail.com>
2023-02-23 05:50:20 -0800

    is alpha

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * int _isupper(int c) - checks if a charcater is upper case or not
- * @c is the testing agent
- * Return 1 0r 0
+ *_isupper - checks if a charcater is upper case or not
+ * @c: is the testing agent
+ * Return: 1 0r 0
  */
 
 int _isupper(int c)
