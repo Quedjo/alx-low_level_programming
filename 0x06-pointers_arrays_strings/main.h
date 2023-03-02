7a8bc73db0846cd523c0b354f3099edb15495fde
quedjo <quedjofrempong@gmail.com>
2023-03-02 01:41:35 -0800

    updating header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef Main
 #define Main
 
+char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
