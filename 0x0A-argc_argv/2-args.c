4bc63c1a28d5a4be46ed80bc3d2cbc85bda500c4
Quedjo <85486255+Quedjo@users.noreply.github.com>
2023-03-10 11:40:13 +0000

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print each argument passed to program on a line
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc-1)
+  {
+		printf("%s\n", argv[i]);
+    i++
+  }
+	return (0);
+}
